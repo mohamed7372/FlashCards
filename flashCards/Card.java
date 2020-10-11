e8c9fc914d1b9ff856846edd19a3ae88de5f000d
Benrabah Mohamed <benrabahmohamed530@gmail.com>
2020-10-11 12:01:25 +0200

    update2
    input card and defi
    and check if correct defi input

--- flashCards/Card.java
+++ flashCards/Card.java
@@ -1,6 +1,6 @@
 package flashCards;
 
-public class Card {
+class Card {
 	String card;
 	String definition;
 	
@@ -9,6 +9,12 @@ public class Card {
 		this.definition = definition;
 	}
 
+	public String check(String input) {
+		if(input.equals(this.definition))
+			return "Your answer is right!";
+		return "Your answer is wrong...";
+	}
+	
 	@Override
 	public String toString() {
 		return "Card:\n" + card + "\nDefinition:\n" + definition;
--- flashCards/Main.java
+++ flashCards/Main.java
@@ -1,9 +1,15 @@
 package flashCards;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		Card c = new Card("purchase", "buy");
-		System.out.println(c.toString());
+		String card = sc.nextLine();
+		String definition = sc.nextLine();
+		Card c = new Card(card, definition);
+		String input = sc.nextLine();
+		System.out.println(c.check(input));
 	}
 }
